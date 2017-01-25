cf5603b7b206eeb42b7bb7992839dc14f16ccbc9
FD-nask <fedor.pavlodar@gmail.com>
2017-01-25 09:20:30 +0600

    Пример работы с Console.Buffer и перечислением ConsoleColor

---
+++ ConsoleBufer/ConsoleBufer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleBufer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleColor[] colors = {ConsoleColor.Black, ConsoleColor.Cyan, ConsoleColor.DarkCyan, ConsoleColor.DarkGreen,
+            ConsoleColor.DarkRed, ConsoleColor.Magenta, ConsoleColor.Yellow};
+
+                foreach (ConsoleColor color in colors)
+            {
+                Console.CursorLeft = (Console.BufferWidth - color.ToString().Length) / 2;
+                Console.CursorTop = 10;
+                Console.ForegroundColor = color;
+                Console.WriteLine(color);
+                Thread.Sleep(800);
+                Console.Clear();
+            }
+        }
+    }
+}
